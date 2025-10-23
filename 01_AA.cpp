242827f4201ba23dbcee7cc5c8a0ff0f03bf9426
monuxyz <monuxchauhan@gmail.com>
2025-10-23 22:53:20 +0530

    Cpp Setup

---
+++ 01_AA.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+
+int main() {
+    fast_io;
+    int a,b,c;
+    cin>>a>>b>>c;
+    cout<<a+1<<b+2<<c+3;
+    
+    return 0;
+}
